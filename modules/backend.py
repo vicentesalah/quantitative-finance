1fca22c912e56ae7e3a9f180ff6ebd5d029cba5d
vicentesalah <salahvicente4@gmail.com>
2025-12-02 14:27:29 +0000

    rama actualizada

--- modules/backend.py
+++ modules/backend.py
@@ -240,3 +240,27 @@ def market_prices(start_date: str=None, end_date: str=None, tickers: list=None) 
     df = db.execute_query(query)
     
     return df
+
+def tickers_by_issuer(issuer:str) -> pd.DataFrame:
+    """
+    Retorna el listado de instrumentos según el emisor indicado
+
+    issuer (str):
+        Emisor
+
+    Return (pd.DataFrame):
+        Tickers del emisor
+    """
+
+    query = f"""
+    SELECT DISTINCT
+    EMISOR, TICKER, CATEGORIA
+    FROM DM_ISTRUMENTOS
+    WHERE EMISOR = "{issuer}"
+    """
+
+    db = DB_Investments()
+    df = db.execute_query(query)
+
+    return df
+
