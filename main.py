c9527db9d61e277de0037cf02350e47cdad5364d
vicentesalah <salahvicente4@gmail.com>
2025-12-03 15:15:23 +0000

    rama actualizada

--- main.py
+++ main.py
@@ -1,32 +1,57 @@
+import pandas as pd
 import numpy as np
 from modules.financials_functions import portfolio_volatility, portfolio_returns, VaR
+from modules.backend import tickers_by_issuer
 
 if __name__ == "__main__":
     
-    # Datos del portafolio
-    tickers = ["IEF", "SPTL", "TLT", "VGLT"]
-    start = "2023-01-01"
+    # Obtener tickers de iShares
+    tickers = tickers_by_issuer(issuer="iShares")
+
+    # Portafolio renta fija
+    tickers_rf = tickers[tickers["CATEGORIA"] == "ETF RF"]
+    list_tickers_rf = list(tickers_rf["TICKER"])
+
+    # Portafolio renta variable
+    tickers_rv = tickers[tickers["CATEGORIA"] == "ETF RV"]
+    list_tickers_rv = list(tickers_rv["TICKER"])
+
+    # Rango de fechas
+    start = "2024-01-01"
     end = "2024-12-31"
 
-    # Descargar retornos del portafolio
-    df = portfolio_returns(
-        tickers=tickers,
-        start=start,
-        end=end
-        )
-    print(df.head(5))
-
-    # Calculo volatilidad
-    vector_w = np.array([1/len(tickers)] * len(tickers))
-    sigma = portfolio_volatility(
-        df=df,
-        vector_w=vector_w
-    )
-    print(sigma)
-    
-    print("="*100)
-
-    # Value at Risk
+    # Nivel de confianza
     confidence = 0.05
-    var = VaR(sigma=sigma, confidence=confidence)
-    print(var)
+    lst = []
+
+    for portafolio in [list_tickers_rf, list_tickers_rv]:
+        
+        # Obtener retornos
+        df = portfolio_returns(tickers = portafolio, start = start, end = end)
+        vector_w = np.array([1/len(portafolio)] * len(portafolio))
+
+        # Calcular volatilidad
+        sigma = portfolio_volatility(df = df, vector_w = vector_w)
+
+        # Calcular VaR
+        var = VaR(sigma = sigma, confidence = confidence)
+        var = np.abs(var)
+        var_mensual = var * np.sqrt(20)
+        lst.append(var_mensual)
+
+    df_final = pd.DataFrame(
+        {"PORTAFOLIO": 
+        ["Ishares Renta Fija",
+        "Ishares Renta Variable"],
+        f"Value at Risk: {1-confidence}%" : lst}
+    )
+    
+    df_final = df_final.sort_values(
+        by = f"Value at Risk: {1-confidence}%",
+            ascending = False)
+    print(df_final)
+
+
+
+    
+    
